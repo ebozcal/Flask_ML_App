18e6e910dfdedfa47e7ceb79809ce8227f5bc142
Ertugrul Bozcal <ebozcal82@gmail.com>
2023-03-24 20:29:32 +0000

    updating predict file

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://mywebapp298797.azurewebsites.net:$PORT/predict 
+     -X POST https://webapp298797.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
